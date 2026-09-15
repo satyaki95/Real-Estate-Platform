5c593293b0d247a0f94d66e0749415b28e9f50ca
satyaki95 <www.satyaki95@gmail.com>
2026-09-15 11:53:27 +0530

    filters and property listing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/shared/LandingPage";
+import Properties from "./pages/shared/Properties";
+import PropertyDetails from "./pages/shared/PropertyDetails";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/properties" element={<Properties />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
       </Routes>
     </div>
   );
