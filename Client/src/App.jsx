274c16c02afbaaa54a58e95234c90f300eda63ca
satyaki95 <www.satyaki95@gmail.com>
2026-09-16 20:40:50 +0530

    login page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,11 +4,13 @@ import Properties from "./pages/shared/Properties";
 import PropertyDetails from "./pages/shared/PropertyDetails";
 import Register from "./pages/auth/Register";
 import VerifyEmail from "./pages/auth/VerifyEmail";
+import Login from "./pages/auth/Login";
 
 const App = () => {
   return (
     <div>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
         <Route path="/" element={<LandingPage />} />
