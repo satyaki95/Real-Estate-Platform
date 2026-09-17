4aeb696a80f713cfc3b9c6349adfa08136cbe6e4
satyaki95 <www.satyaki95@gmail.com>
2026-09-17 19:32:34 +0530

    profile page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,6 +7,7 @@ import VerifyEmail from "./pages/auth/VerifyEmail";
 import Login from "./pages/auth/Login";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import ResetPassword from "./pages/auth/ResetPassword";
+import Profile from "./pages/shared/Profile";
 
 const App = () => {
   return (
@@ -21,6 +22,8 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/properties" element={<Properties />} />
         <Route path="/property/:id" element={<PropertyDetails />} />
+
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
