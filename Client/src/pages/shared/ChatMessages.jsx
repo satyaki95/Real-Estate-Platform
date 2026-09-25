4db5f4016a5d57a45c6ddbd50a128dbae53b51b3
satyaki95 <www.satyaki95@gmail.com>
2026-09-25 16:27:38 +0530

    chat message

--- Client/src/pages/shared/ChatMessages.jsx
+++ Client/src/pages/shared/ChatMessages.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-unused-vars */
 import { useLocation } from "react-router-dom";
 import { chatMessagesStyles as s } from "../../assets/dummyStyles";
 import { useAuth } from "../../context/AuthContext";
@@ -8,7 +7,12 @@ import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import API_URL from "../../config";
 import Navbar from "../../components/common/Navbar";
-import { HiOutlineChatAlt2, HiOutlineTrash } from "react-icons/hi";
+import {
+  HiChevronLeft,
+  HiOutlineChatAlt2,
+  HiOutlineTrash,
+  HiPaperAirplane,
+} from "react-icons/hi";
 
 const ChatMessages = () => {
   const { user, token } = useAuth();
@@ -238,6 +242,97 @@ const ChatMessages = () => {
               ))
             )}
           </div>
+        </div>
+
+        {/* main chat area */}
+        <div className={s.chatArea}>
+          {activeChat ? (
+            <>
+              <div className={s.chatHeader}>
+                <div className={s.chatHeaderLeft}>
+                  <button
+                    className={s.backButton}
+                    onClick={() => setActiveChat(null)}
+                  >
+                    <HiChevronLeft size={24} />
+                  </button>
+                  <div className={s.avatar}>
+                    {getChatPartner(activeChat)?.profilePic ? (
+                      <img
+                        className={s.avatarImg}
+                        src={getChatPartner(activeChat).profilePic}
+                        alt=""
+                      />
+                    ) : (
+                      getChatPartner(activeChat)?.name?.charAt(0)
+                    )}
+                  </div>
+                  <div className={s.chatPartnerName}>
+                    {getChatPartner(activeChat)?.name}
+                  </div>
+                </div>
+              </div>
+
+              <div className={s.messagesArea}>
+                {messages.map((msg, idx) => (
+                  <div
+                    key={idx}
+                    className={`${s.messageBubble} ${(msg.sender?._id || msg.sender) === user._id ? s.messageOwn : s.messageOther}`}
+                  >
+                    <div className={s.messageContent}>
+                      {msg.image && (
+                        <div className={s.messageImageWrapper}>
+                          <img
+                            src={msg.image}
+                            alt="Property Reference"
+                            className={s.messageImage}
+                          />
+                        </div>
+                      )}
+                      <div className={s.messageText}>{msg.text}</div>
+                      {(msg.sender?._id || msg.sender) === user._id && (
+                        <button
+                          className={s.deleteMessageButton}
+                          onClick={() =>
+                            handleDeleteMessage(activeChat._id, msg._id)
+                          }
+                          title="Delete Message"
+                        >
+                          <HiOutlineTrash size={14} />
+                        </button>
+                      )}
+                    </div>
+                    <span className={s.messageTime}>
+                      {new Date(msg.createdAt).toLocaleTimeString([], {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}
+                    </span>
+                  </div>
+                ))}
+                <div ref={messagesEndRef} />
+              </div>
+
+              <form className={s.messageForm} onSubmit={handleSendMessage}>
+                <input
+                  type="text"
+                  className={s.messageInput}
+                  placeholder="Type a message..."
+                  value={newMessage}
+                  onChange={(e) => setNewMessage(e.target.value)}
+                />
+                <button type="submit" className={s.sendButton}>
+                  <HiPaperAirplane className={s.sendIcon} />
+                </button>
+              </form>
+            </>
+          ) : (
+            <div className={s.noChatSelected}>
+              <HiOutlineChatAlt2 className={s.noChatIcon} />
+              <h3 className={s.noChatTitle}>Your Messages</h3>
+              <p>Select a conversation to start chatting</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
