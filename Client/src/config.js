639e88349c55b64c1afe0fe9cb88cd853e5f083f
satyaki95 <www.satyaki95@gmail.com>
2026-09-26 15:51:05 +0530

    update

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 // Base URL for the backend API used by all frontend requests.
 // This keeps the client code clean and centralized instead of hardcoding the server URL everywhere.
 const API_URL = import.meta.env.VITE_LOCAL_API_URL;
